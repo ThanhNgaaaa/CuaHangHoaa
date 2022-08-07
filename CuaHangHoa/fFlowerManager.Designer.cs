4416b83b34891f57f7056db0baa712656b956064
tranviet741 <tranviet741@gmail.com>
2022-08-07 13:43:09 +0700

    đỏi form main

--- CuaHangHoa/fFlowerManager.Designer.cs
+++ CuaHangHoa/fFlowerManager.Designer.cs
@@ -72,7 +72,7 @@
             // menuStrip2
             // 
             this.menuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip2.Location = new System.Drawing.Point(0, 28);
+            this.menuStrip2.Location = new System.Drawing.Point(0, 24);
             this.menuStrip2.Name = "menuStrip2";
             this.menuStrip2.Size = new System.Drawing.Size(1375, 24);
             this.menuStrip2.TabIndex = 0;
@@ -87,7 +87,7 @@
             this.adminToolStripMenuItem});
             this.menuStrip3.Location = new System.Drawing.Point(0, 0);
             this.menuStrip3.Name = "menuStrip3";
-            this.menuStrip3.Size = new System.Drawing.Size(1375, 28);
+            this.menuStrip3.Size = new System.Drawing.Size(1375, 24);
             this.menuStrip3.TabIndex = 1;
             this.menuStrip3.Text = "menuStrip3";
             // 
@@ -97,20 +97,20 @@
             this.toolStripMenuItemThongTinTaiKhoan,
             this.toolStripMenuItemDangXuat});
             this.toolStripMenuItemTaiKhoan.Name = "toolStripMenuItemTaiKhoan";
-            this.toolStripMenuItemTaiKhoan.Size = new System.Drawing.Size(85, 24);
+            this.toolStripMenuItemTaiKhoan.Size = new System.Drawing.Size(69, 20);
             this.toolStripMenuItemTaiKhoan.Text = "Tài khoản";
             // 
             // toolStripMenuItemThongTinTaiKhoan
             // 
             this.toolStripMenuItemThongTinTaiKhoan.Name = "toolStripMenuItemThongTinTaiKhoan";
-            this.toolStripMenuItemThongTinTaiKhoan.Size = new System.Drawing.Size(224, 26);
+            this.toolStripMenuItemThongTinTaiKhoan.Size = new System.Drawing.Size(180, 22);
             this.toolStripMenuItemThongTinTaiKhoan.Text = "Thông tin tài khoản ";
             this.toolStripMenuItemThongTinTaiKhoan.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
             // 
             // toolStripMenuItemDangXuat
             // 
             this.toolStripMenuItemDangXuat.Name = "toolStripMenuItemDangXuat";
-            this.toolStripMenuItemDangXuat.Size = new System.Drawing.Size(224, 26);
+            this.toolStripMenuItemDangXuat.Size = new System.Drawing.Size(180, 22);
             this.toolStripMenuItemDangXuat.Text = "Đăng xuất";
             this.toolStripMenuItemDangXuat.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
             // 
@@ -122,42 +122,42 @@
             this.cậpNhậtGiáToolStripMenuItem,
             this.KhachHangtoolStripMenuItem2});
             this.quảnLýToolStripMenuItem.Name = "quảnLýToolStripMenuItem";
-            this.quảnLýToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.quảnLýToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
             this.quảnLýToolStripMenuItem.Text = "Quản lý";
             this.quảnLýToolStripMenuItem.Click += new System.EventHandler(this.quảnLýToolStripMenuItem_Click);
             // 
             // hàngHóaToolStripMenuItem
             // 
             this.hàngHóaToolStripMenuItem.Name = "hàngHóaToolStripMenuItem";
-            this.hàngHóaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.hàngHóaToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
             this.hàngHóaToolStripMenuItem.Text = "Hàng hóa";
             this.hàngHóaToolStripMenuItem.Click += new System.EventHandler(this.hàngHóaToolStripMenuItem_Click);
             // 
             // tìmKiếmHàngHóaToolStripMenuItem
             // 
             this.tìmKiếmHàngHóaToolStripMenuItem.Name = "tìmKiếmHàngHóaToolStripMenuItem";
-            this.tìmKiếmHàngHóaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.tìmKiếmHàngHóaToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
             this.tìmKiếmHàngHóaToolStripMenuItem.Text = "Tìm kiếm hàng hóa";
             this.tìmKiếmHàngHóaToolStripMenuItem.Click += new System.EventHandler(this.tìmKiếmHàngHóaToolStripMenuItem_Click);
             // 
             // cậpNhậtGiáToolStripMenuItem
             // 
             this.cậpNhậtGiáToolStripMenuItem.Name = "cậpNhậtGiáToolStripMenuItem";
-            this.cậpNhậtGiáToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.cậpNhậtGiáToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
             this.cậpNhậtGiáToolStripMenuItem.Text = "Cập nhật giá";
             this.cậpNhậtGiáToolStripMenuItem.Click += new System.EventHandler(this.cậpNhậtGiáToolStripMenuItem_Click);
             // 
             // KhachHangtoolStripMenuItem2
             // 
             this.KhachHangtoolStripMenuItem2.Name = "KhachHangtoolStripMenuItem2";
-            this.KhachHangtoolStripMenuItem2.Size = new System.Drawing.Size(224, 26);
+            this.KhachHangtoolStripMenuItem2.Size = new System.Drawing.Size(176, 22);
             this.KhachHangtoolStripMenuItem2.Text = "Khách Hàng";
             this.KhachHangtoolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
             // 
             // adminToolStripMenuItem
             // 
             this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
-            this.adminToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
+            this.adminToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
             this.adminToolStripMenuItem.Text = "Admin";
             this.adminToolStripMenuItem.Click += new System.EventHandler(this.adminToolStripMenuItem_Click);
             // 
@@ -167,7 +167,7 @@
             this.label5.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(398, 65);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(615, 32);
+            this.label5.Size = new System.Drawing.Size(488, 25);
             this.label5.TabIndex = 0;
             this.label5.Text = "CHÀO MỪNG BẠN ĐẾN VỚI CỬA HÀNG HOA";
             this.label5.Click += new System.EventHandler(this.label5_Click);
@@ -177,7 +177,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(855, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 23);
+            this.label1.Size = new System.Drawing.Size(49, 19);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
@@ -187,7 +187,7 @@
             this.label6.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label6.Location = new System.Drawing.Point(926, 613);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(115, 25);
+            this.label6.Size = new System.Drawing.Size(93, 22);
             this.label6.TabIndex = 14;
             this.label6.Text = "Tổng tiền:";
             // 
@@ -197,7 +197,7 @@
             this.txtTongTien.Location = new System.Drawing.Point(1047, 610);
             this.txtTongTien.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtTongTien.Name = "txtTongTien";
-            this.txtTongTien.Size = new System.Drawing.Size(242, 34);
+            this.txtTongTien.Size = new System.Drawing.Size(242, 29);
             this.txtTongTien.TabIndex = 15;
             // 
             // btnHuyHD
@@ -210,6 +210,7 @@
             this.btnHuyHD.TabIndex = 20;
             this.btnHuyHD.Text = "Hủy hóa đơn";
             this.btnHuyHD.UseVisualStyleBackColor = true;
+            this.btnHuyHD.Click += new System.EventHandler(this.btnHuyHD_Click);
             // 
             // btnLuu
             // 
@@ -221,6 +222,7 @@
             this.btnLuu.TabIndex = 19;
             this.btnLuu.Text = "Lưu hóa đơn";
             this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
             // 
             // btnThemHD
             // 
@@ -232,6 +234,7 @@
             this.btnThemHD.TabIndex = 18;
             this.btnThemHD.Text = "Thêm hóa đơn";
             this.btnThemHD.UseVisualStyleBackColor = true;
+            this.btnThemHD.Click += new System.EventHandler(this.btnThemHD_Click);
             // 
             // dtgv_HoaDon
             // 
@@ -271,6 +274,7 @@
             this.btnThem.TabIndex = 11;
             this.btnThem.Text = "Thêm hoa";
             this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
             // 
             // label2
             // 
@@ -278,7 +282,7 @@
             this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label2.Location = new System.Drawing.Point(558, 38);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(114, 25);
+            this.label2.Size = new System.Drawing.Size(93, 22);
             this.label2.TabIndex = 5;
             this.label2.Text = "Số lượng :";
             // 
@@ -288,7 +292,7 @@
             this.nmSoLuong.Location = new System.Drawing.Point(708, 37);
             this.nmSoLuong.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.nmSoLuong.Name = "nmSoLuong";
-            this.nmSoLuong.Size = new System.Drawing.Size(119, 34);
+            this.nmSoLuong.Size = new System.Drawing.Size(119, 29);
             this.nmSoLuong.TabIndex = 4;
             // 
             // label7
@@ -297,7 +301,7 @@
             this.label7.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label7.Location = new System.Drawing.Point(40, 37);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(98, 25);
+            this.label7.Size = new System.Drawing.Size(78, 22);
             this.label7.TabIndex = 3;
             this.label7.Text = "Mã hoa:";
             // 
@@ -308,7 +312,7 @@
             this.cmbMaHoa.Location = new System.Drawing.Point(203, 34);
             this.cmbMaHoa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmbMaHoa.Name = "cmbMaHoa";
-            this.cmbMaHoa.Size = new System.Drawing.Size(303, 34);
+            this.cmbMaHoa.Size = new System.Drawing.Size(303, 29);
             this.cmbMaHoa.TabIndex = 2;
             // 
             // groupBox1
@@ -336,7 +340,7 @@
             this.label4.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label4.Location = new System.Drawing.Point(530, 83);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(108, 25);
+            this.label4.Size = new System.Drawing.Size(88, 22);
             this.label4.TabIndex = 7;
             this.label4.Text = "Ngày lập:";
             // 
@@ -347,7 +351,7 @@
             this.dtpNgayLap.Location = new System.Drawing.Point(726, 83);
             this.dtpNgayLap.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dtpNgayLap.Name = "dtpNgayLap";
-            this.dtpNgayLap.Size = new System.Drawing.Size(352, 34);
+            this.dtpNgayLap.Size = new System.Drawing.Size(352, 29);
             this.dtpNgayLap.TabIndex = 6;
             // 
             // txtMaHD
@@ -356,7 +360,7 @@
             this.txtMaHD.Location = new System.Drawing.Point(203, 78);
             this.txtMaHD.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtMaHD.Name = "txtMaHD";
-            this.txtMaHD.Size = new System.Drawing.Size(293, 34);
+            this.txtMaHD.Size = new System.Drawing.Size(293, 29);
             this.txtMaHD.TabIndex = 5;
             // 
             // label3
@@ -365,7 +369,7 @@
             this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label3.Location = new System.Drawing.Point(29, 76);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(143, 25);
+            this.label3.Size = new System.Drawing.Size(114, 22);
             this.label3.TabIndex = 4;
             this.label3.Text = "Mã hóa đơn:";
             // 
@@ -375,7 +379,7 @@
             this.label8.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label8.Location = new System.Drawing.Point(530, 33);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(178, 25);
+            this.label8.Size = new System.Drawing.Size(143, 22);
             this.label8.TabIndex = 3;
             this.label8.Text = "Mã khách hàng:";
             // 
@@ -386,7 +390,7 @@
             this.cmbMaKh.Location = new System.Drawing.Point(724, 29);
             this.cmbMaKh.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmbMaKh.Name = "cmbMaKh";
-            this.cmbMaKh.Size = new System.Drawing.Size(354, 34);
+            this.cmbMaKh.Size = new System.Drawing.Size(354, 29);
             this.cmbMaKh.TabIndex = 2;
             // 
             // txtMaNv
@@ -395,7 +399,7 @@
             this.txtMaNv.Location = new System.Drawing.Point(203, 30);
             this.txtMaNv.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtMaNv.Name = "txtMaNv";
-            this.txtMaNv.Size = new System.Drawing.Size(293, 34);
+            this.txtMaNv.Size = new System.Drawing.Size(293, 29);
             this.txtMaNv.TabIndex = 1;
             // 
             // label9
@@ -404,7 +408,7 @@
             this.label9.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label9.Location = new System.Drawing.Point(29, 29);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(157, 25);
+            this.label9.Size = new System.Drawing.Size(126, 22);
             this.label9.TabIndex = 0;
             this.label9.Text = "Mã nhân viên:";
             // 
--- CuaHangHoa/fFlowerManager.cs
+++ CuaHangHoa/fFlowerManager.cs
@@ -14,11 +14,17 @@ namespace CuaHangHoa
    
     public partial class FlowerManager : Form
     {
+
+
+        private DataTable tblCTHD;
+
         SqlConnection connection;
         string loaiTK = fDangnhap.LOAITK_USER;
         string nv = "1";
-      
-       
+
+        private string maNV = fDangnhap.MA_USER;
+
+
         public FlowerManager()
         {
             InitializeComponent();
@@ -28,14 +34,35 @@ namespace CuaHangHoa
         
         private void FlowerManager_Load(object sender, EventArgs e)
         {
-            label1.Text = "XIN CHÀO : " + Variables.TenNV;
+            label1.Text = "Xin chào người dùng : " + Variables.TenNV;
+
+           
+
             String conn = ConfigurationManager.ConnectionStrings["QLHOA"].ConnectionString.ToString();
-            connection= new SqlConnection(conn);
+            connection = new SqlConnection(conn);
             connection.Open();
-            if ( loaiTK.Equals(nv))
+
+
+
+
+            if (loaiTK.Equals(nv))
             {
                 adminToolStripMenuItem.Enabled = false;
             }
+
+
+
+            btnThemHD.Enabled = true;
+            btnThem.Enabled = false;
+            btnLuu.Enabled = false;
+            btnHuyHD.Enabled = false;
+            txtMaNv.Text = maNV;
+            txtTongTien.Text = "0";
+            FillCombo("SELECT MaHoa FROM Hoa", cmbMaHoa, "MaHoa", "TenHoa");
+            cmbMaHoa.SelectedIndex = -1;
+            FillCombo("SELECT MaKh, TenKh FROM KhachHang", cmbMaKh, "MaKh", "TenKh");
+            cmbMaKh.SelectedIndex = -1;
+            LoadDataGridView();
         }
 
         private void toolStripMenuItem4_Click(object sender, EventArgs e)
@@ -92,6 +119,288 @@ namespace CuaHangHoa
         private void label5_Click(object sender, EventArgs e)
         {
 
+        }
+
+
+
+
+
+
+
+        public void FillCombo(string sql, ComboBox cbo, string ma, string ten)
+        {
+            SqlDataAdapter dap = new SqlDataAdapter(sql, connection);
+            DataTable table = new DataTable();
+            dap.Fill(table);
+            cbo.BindingContext = this.BindingContext;
+            cbo.DataSource = table;
+            cbo.ValueMember = ma; //Trường giá trị
+            cbo.DisplayMember = ten; //Trường hiển thị
+        }
+
+        private void btnThemHD_Click(object sender, EventArgs e)
+        {
+            btnThem.Enabled = true;
+            btnLuu.Enabled = true;
+            btnHuyHD.Enabled = true;
+            if (KiemTraThongTin())
+            {
+                themHoaDon();
+            }
+            LoadDataGridView();
+        }
+        private bool KiemTraThongTin()
+        {
+            if (txtMaHD.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập mã hóa đơn", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtMaHD.Focus();
+                return false;
+            }
+            if (txtMaNv.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập mã nhân viên", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtMaNv.Focus();
+                return false;
+            }
+            if (cmbMaKh.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập mã khách hàng", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cmbMaKh.Focus();
+                return false;
+            }
+            if (cmbMaHoa.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập mã hoa", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cmbMaHoa.Focus();
+                return false;
+            }
+            if (nmSoLuong.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập số lượng", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                nmSoLuong.Focus();
+                return false;
+            }
+            return true;
+        }
+
+
+        private void themHoaDon()
+        {
+            if (KiemTraPrimaryBill())
+            {
+                btnThemHD.Enabled = true;
+                string sqlThemHD = "insert into HoaDon(MaHoaDon,MaNv,MaKh,NgayLap) values(@MaHoaDon,@MaNv, @MaKh, @NgayLap)";
+                SqlCommand command = new SqlCommand(sqlThemHD, connection);
+                command.Parameters.AddWithValue("MaHoaDon", txtMaHD.Text);
+                command.Parameters.AddWithValue("MaNv", txtMaNv.Text);
+                command.Parameters.AddWithValue("MaKh", cmbMaKh.SelectedValue);
+                command.Parameters.AddWithValue("NgayLap", dtpNgayLap.Value);
+                command.ExecuteNonQuery();
+            }
+
+        }
+
+
+        private bool KiemTraPrimaryBill()
+        {
+            string sql = "SELECT MaHoaDon FROM HoaDon where MaHoaDon ='" + txtMaHD.Text + "'";
+            if (CheckKey(sql))
+            {
+                MessageBox.Show("Mã hóa đơn này đã có, bạn phải nhập mã khác", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtMaHD.Text = "";
+                txtMaHD.Focus();
+                return false;
+            }
+            return true;
+        }
+
+
+
+        public bool CheckKey(string sql)
+        {
+            SqlDataAdapter dap = new SqlDataAdapter(sql, connection);
+            DataTable table = new DataTable();
+            dap.Fill(table);
+            if (table.Rows.Count > 0)
+                return true;
+            else return false;
+        }
+
+
+        private void LoadDataGridView()
+        {
+            string sql;
+            sql = "select L.TenLoai, H.MaHoa, H.TenHoa, C.SoLuong, H.GiaBan, C.SoLuong*H.GiaBan as [Tổng tiền] from ChiTietHoaDon C, Hoa H, LoaiHoa L where L.MaLoai = H.MaLoai and  C.MaHoa = H.MaHoa and C.MaHoaDon = '" + txtMaHD.Text + "'";
+            tblCTHD = GetDataToTable(sql);
+            dtgv_HoaDon.DataSource = tblCTHD;
+            dtgv_HoaDon.Columns[0].HeaderText = "Loại hoa";
+            dtgv_HoaDon.Columns[1].HeaderText = "Mã hoa";
+            dtgv_HoaDon.Columns[2].HeaderText = "Tên hoa";
+            dtgv_HoaDon.Columns[3].HeaderText = "Số lượng";
+            dtgv_HoaDon.Columns[4].HeaderText = "Đơn giá";
+            dtgv_HoaDon.Columns[5].HeaderText = "Tổng tiền";
+            dtgv_HoaDon.Columns[0].Width = 80;
+            dtgv_HoaDon.Columns[1].Width = 80;
+            dtgv_HoaDon.Columns[2].Width = 130;
+            dtgv_HoaDon.Columns[3].Width = 90;
+            dtgv_HoaDon.Columns[4].Width = 90;
+            dtgv_HoaDon.Columns[5].Width = 90;
+            dtgv_HoaDon.AllowUserToAddRows = false;
+            dtgv_HoaDon.EditMode = DataGridViewEditMode.EditProgrammatically;
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            string sql;
+            double sl, SLcon, tong, Tongmoi;
+            sl = Convert.ToDouble(GetFieldValues("SELECT SoLuongTon FROM Hoa WHERE MaHoa = N'" + cmbMaHoa.SelectedValue + "'"));
+            if (Convert.ToDouble(nmSoLuong.Text) > sl)
+            {
+                MessageBox.Show("Số lượng mặt hàng này chỉ còn " + sl, "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                nmSoLuong.Text = "";
+                nmSoLuong.Focus();
+                return;
+            }
+            // Cập nhật lại số lượng của mặt hàng vào bảng tblHang
+            SLcon = sl - Convert.ToDouble(nmSoLuong.Text);
+            sql = "UPDATE Hoa SET SoLuongTon =" + SLcon + " WHERE MaHoa= N'" + cmbMaHoa.SelectedValue + "'";
+
+            ResetValues();
+            btnHuyHD.Enabled = true;
+            btnThem.Enabled = true;
+            MessageBox.Show("Lưu thành công!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+
+        private void ResetValues()
+        {
+            txtMaHD.Text = "";
+            dtpNgayLap.Value = DateTime.Now;
+            txtMaNv.Text = "";
+            cmbMaKh.Text = "";
+            txtTongTien.Text = "0";
+            cmbMaHoa.Text = "";
+            nmSoLuong.Text = "";
+        }
+
+
+
+        public string GetFieldValues(string sql)
+        {
+            string ma = "";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            SqlDataReader reader;
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+                ma = reader.GetValue(0).ToString();
+            reader.Close();
+            return ma;
+        }
+
+        private void btnHuyHD_Click(object sender, EventArgs e)
+        {
+            double sl, slcon, slxoa;
+            if (MessageBox.Show("Bạn có chắc chắn muốn xóa không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                string sql = "SELECT MaHoa,SoLuong FROM ChiTietHoaDon WHERE MaHoaDon = '" + txtMaHD.Text + "'";
+                DataTable tblHang = GetDataToTable(sql);
+                for (int hang = 0; hang <= tblHang.Rows.Count - 1; hang++)
+                {
+                    // Cập nhật lại số lượng cho các mặt hàng
+                    sl = Convert.ToDouble(GetFieldValues("SELECT SoLuongTon FROM Hoa WHERE MaHoa = '" + tblHang.Rows[hang][0].ToString() + "'"));
+                    slxoa = Convert.ToDouble(tblHang.Rows[hang][1].ToString());
+                    slcon = sl + slxoa;
+                    sql = "UPDATE Hoa SET SoLuongTon =" + slcon + " WHERE MaHoa= N'" + tblHang.Rows[hang][0].ToString() + "'";
+                    RunSQL(sql);
+                }
+
+                //Xóa chi tiết hóa đơn
+                sql = "DELETE ChiTietHoaDon WHERE MaHoaDon='" + txtMaHD.Text + "'";
+                RunSQL(sql);
+
+                //Xóa hóa đơn
+                sql = "DELETE HoaDon WHERE MaHoaDon=N'" + txtMaHD.Text + "'";
+                RunSQL(sql);
+                ResetValues();
+                LoadDataGridView();
+                btnHuyHD.Enabled = false;
+                MessageBox.Show("Xóa thành công!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+        }
+
+
+
+        public DataTable GetDataToTable(string sql)
+        {
+            DataTable table = new DataTable();
+            SqlDataAdapter dap = new SqlDataAdapter(sql, connection);
+            dap.Fill(table);
+            return table;
+        }
+
+        public void RunSQL(string sql)
+        {
+            SqlCommand cmd; //Đối tượng thuộc lớp SqlCommand
+            cmd = new SqlCommand();
+            cmd.Connection = connection; //Gán kết nối
+            cmd.CommandText = sql; //Gán lệnh SQL
+            try
+            {
+                cmd.ExecuteNonQuery(); //Thực hiện câu lệnh SQL
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            cmd.Dispose();//Giải phóng bộ nhớ
+            cmd = null;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            btnThemHD.Enabled = true;
+            if (KiemTraPrimaryFlower())
+            {
+                string sqlThemCTHD = "insert into ChiTietHoaDon(MaHoa,MaHoaDon,SoLuong) values(@MaHoa,@MaHoaDon, @SL)";
+                SqlCommand command = new SqlCommand(sqlThemCTHD, connection);
+                command.Parameters.AddWithValue("MaHoa", cmbMaHoa.SelectedValue);
+                command.Parameters.AddWithValue("MaHoaDon", txtMaHD.Text);
+                command.Parameters.AddWithValue("SL", nmSoLuong.Text);
+                command.ExecuteNonQuery();
+                MessageBox.Show("Thêm hoa thành công!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadDataGridView();
+
+            }
+            thanhTien();
+        }
+
+
+        private bool KiemTraPrimaryFlower()
+        {
+            string sql = "SELECT MaHoa FROM ChiTietHoaDon where MaHoaDon ='" + txtMaHD.Text + "' and MaHoa='" + cmbMaHoa.SelectedValue + "'";
+            if (CheckKey(sql))
+            {
+                MessageBox.Show("Mã hoa này đã có trong hóa đơn này, bạn phải nhập mã khác", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cmbMaHoa.Text = "";
+                cmbMaHoa.Focus();
+                return false;
+            }
+            return true;
+        }
+
+
+
+        private void thanhTien()
+        {
+            int sc = dtgv_HoaDon.Rows.Count;
+            float thanhtien = 0;
+            for (int i = 0; i < sc; i++)
+                thanhtien += float.Parse(dtgv_HoaDon.Rows[i].Cells["Tổng tiền"].Value.ToString());
+            txtTongTien.Text = thanhtien.ToString();
         }
     }
 }
